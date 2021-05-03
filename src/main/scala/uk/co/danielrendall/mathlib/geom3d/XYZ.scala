158cb663e1afb1d5b78a6193dfa8dd896badc22d
Daniel Rendall <drendall@gmail.com>
2021-05-03 12:42:06 +0100

    Add the XYZ trait (which was never used in the Java implementation)

---
+++ src/main/scala/uk/co/danielrendall/mathlib/geom3d/XYZ.scala
@@ -0,0 +1,13 @@
+package uk.co.danielrendall.mathlib.geom3d
+
+import uk.co.danielrendall.mathlib.geom2d.XY
+
+/**
+ * @author Daniel Rendall <drendall@gmail.com>
+ * @since 02-Apr-2010 08:35:26
+ */
+trait XYZ extends XY {
+
+  def z: Double
+
+}
